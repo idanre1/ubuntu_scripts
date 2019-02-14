a379ba97b919b752a6464f2842ce5e18796e1b01
Idan Regev <idanre1@gmx.com>
2019-02-14 14:23:47 +0200

    lubuntu

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# Usage: install git libcurl4-openssl-dev
 cd ~
-alias apt-yes='sudo DEBIAN_FRONTEND=noninteractive apt yes -y '
+aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 
 #Git + fetch workspace from git
-apt-yes install git libcurl4-openssl-dev
+$aptyes install git libcurl4-openssl-dev
 git clone https://github.com/idanre1/settings.git
 git clone https://github.com/idanre1/scripts.git
 git clone https://github.com/idanre1/ubuntu_scripts.git
@@ -12,10 +13,10 @@ git clone https://github.com/idanre1/ubuntu_scripts.git
 sudo ln -s ~ /nas
 sudo ln -s /home/$USER /home/idan
 
-apt-yes install source-highlight
+$aptyes install source-highlight
 echo source ~/settings/bashrc >> ~/.bashrc
 echo source ~/ubuntu_scripts/python_init.sh >> ~/.bashrc
-#apt-yes install nmon # htop nmon.sourceforge.net
+#$aptyes install nmon # htop nmon.sourceforge.net
 
 # aarc64 to x86 patch
 chmod +x ~/scripts/*
@@ -23,7 +24,7 @@ chmod +x ~/scripts/*
 ln -s ~/ubuntu_scripts/hub ~/scripts/hub 
 
 # Ubuntu specific
-#apt-yes install git rclone
+#$aptyes install git rclone
 #mkdir ~/onedrive
 
 # Setting python
@@ -33,3 +34,12 @@ pip3 install glances # htop
 # TODOS:
 echo "For your choice:"
 echo "sudo apt-get install openssh-server"
+
+# Trailer
+echo "Todos:"
+echo "always-on, qutologin"
+echo "update upgrade dist-upgrade"
+echo "crome remote"
+echo "Dropbox"
+echo "Visual studio code"
+echo "TWS"
--- python_install.sh
+++ python_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 deactivate
 
-apt-yes install python3 python3-pip python3-tk virtualenv
-virtualenv -p /usr/bin/python3 --no-site-packages py3env
-virtualenv -p /usr/bin/python2 --no-site-packages py2env
+aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
+$aptyes install python3 python3-pip python3-tk virtualenv
+virtualenv -p /usr/bin/python3 --no-site-packages ~/py3env
+virtualenv -p /usr/bin/python2 --no-site-packages ~/py2env
 
 source ~/ubuntu_scripts/python_init.sh
 
@@ -24,8 +25,3 @@ pip install numpy pandas
 
 deactivate
 
-# Trailer
-echo "Todos:"
-echo "Dropbox"
-echo "Visual studio code"
-echo "TWS"
