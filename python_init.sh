9e911c09c329b562554aff0923603d5164298e8f
Idan Regev <idanre1@gmx.com>
2019-05-28 17:09:56 +0300

    sss

--- python_init.sh
+++ python_init.sh
@@ -4,6 +4,6 @@ alias py3='/usr/bin/python3'
 py2bin=/home/idan/py2env/bin
 py3bin=/home/idan/py3env/bin
 
-alias py2env='deactivate; cd $py2bin; source activate; cd -'
-alias py3env='deactivate; cd $py3bin; source activate; cd -'
+alias py2env='cd $py2bin; source activate; cd -'
+alias py3env='cd $py3bin; source activate; cd -'
 
