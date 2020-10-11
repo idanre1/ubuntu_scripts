033a3ade106e2c19ca86b0d113983d66c7dda421
Ubuntu <idanre1@orchestrator.q14jdk54dkcelgkaslmxmd0yrg.bx.internal.cloudapp.net>
2020-10-11 10:19:45 +0000

    Docker compose

--- install_orchestrator.sh
+++ install_orchestrator.sh
@@ -60,6 +60,10 @@ sudo add-apt-repository \
   $(lsb_release -cs) \
   stable"
 $aptyes install docker-ce docker-ce-cli containerd.io
+# Docker compose container
+sudo curl -L --fail https://github.com/docker/compose/releases/download/1.27.4/run.sh -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 
 # ###################################################################
 # TODOS:
