00b9e26ed3f471b56bc9558bfc97a7c216fe12bd
idanre1 <idanre1@gmx.com>
2020-12-18 15:42:50 +0200

    one install master

--- install_windows10_ubuntu.sh
+++ install_windows10_ubuntu.sh
@@ -3,31 +3,9 @@
 cd ~
 aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 
-# Update machine
-sudo apt-get update
-
-#Git + fetch workspace from git
-$aptyes install git libcurl4-openssl-dev
-git clone https://github.com/idanre1/settings.git
-git clone https://github.com/idanre1/scripts.git
 git clone https://github.com/idanre1/ubuntu_scripts.git
 
-#Making nice linux
-sudo ln -s ~ /nas
-sudo ln -s /home/$USER /home/idan
-
-$aptyes install source-highlight curl
-echo source ~/settings/bashrc >> ~/.bashrc
-
-# aarc64 to x86 patch
-chmod +x ~/scripts/*
-\rm -rf ~/scripts/hub
-ln -s ~/ubuntu_scripts/hub ~/scripts/hub 
-
 # Jupyter
-$aptyes install python3 python3-pip
-$aptyes install jupyter-notebook
-
 pip3 install numpy==1.11.3 pandas==0.18.1
 pip3 install pyfolio
 
