43eb79640a2482213132a7d8a79c3677c2cec0aa
idanre1 <idanre1@gmx.com>
2020-12-18 15:39:16 +0200

    one install master

--- install_orchestrator.sh
+++ install_orchestrator.sh
@@ -1,37 +1,17 @@
 #!/bin/bash
-# Usage: install git libcurl4-openssl-dev
-#        git clone https://github.com/idanre1/ubuntu_scripts.git
+# Usage: wget https://raw.githubusercontent.com/idanre1/ubuntu_scripts/master/install_orchestrator.sh
 cd ~
 aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 
-$aptyes update
-
-#Git + fetch workspace from git
-$aptyes install git libcurl4-openssl-dev
-git clone https://github.com/idanre1/settings.git
-git clone https://github.com/idanre1/scripts.git
 git clone https://github.com/idanre1/ubuntu_scripts.git
 
 #Making nice linux
-sudo ln -s ~ /nas
-sudo ln -s /home/$USER /home/idan
 ln -fs /home/$USER/orchestrator /home/$USER/algo
-# set timezone
-sudo timedatectl set-timezone Asia/Jerusalem
 
-$aptyes install source-highlight curl
-echo source ~/settings/bashrc >> ~/.bashrc
-echo source ~/settings/vimrc >> ~/.vimrc
-echo source ~/ubuntu_scripts/python_init.sh >> ~/.bashrc
 #$aptyes install nmon # htop nmon.sourceforge.net
 
 # virtual X server
 # $aptyes xserver-xorg-video-dummy
-
-# aarc64 to x86 patch
-chmod +x ~/scripts/*
-\rm -rf ~/scripts/hub
-ln -s ~/ubuntu_scripts/hub ~/scripts/hub 
 
 # Setting python
 ~/ubuntu_scripts/python_install_orchestrator.sh
