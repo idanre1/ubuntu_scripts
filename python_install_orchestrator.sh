d665f2b28d5e3030ee6688892cfc00dc993d0736
Idan Regev <idanre1@gmx.com>
2022-06-08 08:22:25 +0300

    Update python_install_orchestrator.sh

--- python_install_orchestrator.sh
+++ python_install_orchestrator.sh
@@ -3,7 +3,7 @@ deactivate
 
 aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 $aptyes install python3 python3-pip python3-tk virtualenv
-virtualenv -p /usr/bin/python3 --no-site-packages ~/py3env
+virtualenv -p /usr/bin/python3.8 --no-site-packages ~/Envs/py3env
 
 source ~/ubuntu_scripts/python_init.sh
 
@@ -13,17 +13,22 @@ source ~/ubuntu_scripts/python_init.sh
 cd $py3bin
 source activate
 pip -V
-pip install python-dateutil # parse iso format dates before python 3.7
+# pip install python-dateutil # parse iso format dates before python 3.7
 
 pip install zmq #pylint
 
 # scrapper
 # pip install aiohttp fake_useragent
 # pip install scrapy-mongodb
-pip install numpy pandas
+pip install PyYAML
+pip install numpy pandas pandas-market-calendars
 pip install apscheduler SQLAlchemy dateutil
 pip install lxml fake-useragent
 
+pip install dvc[azure] pyAesCrypt
+
 pip install finam-export
+pip install alpaca_trade_api
+pip install mplfinance
 
 deactivate
